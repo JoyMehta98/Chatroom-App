6414c878b9d611c7f3c258fdfedf2ba47a58b7f2
JoyMehta98 <joymehta1998@gmail.com>
2020-06-16 17:25:25 +0530

    laoding data isuue done

--- main/consumers.py
+++ main/consumers.py
@@ -12,12 +12,13 @@ class ChatConsumer(WebsocketConsumer):
     def fetch_messages(self , data):
         print("Fetch")
         messages = Message.last_10_messages()
-        print(messages)
-        print(list(messages))
+        # print(messages)
+        # print(list(messages))
         content = {
             'command' : 'messages',
             'messages' : self.messages_to_json(messages)
         }
+        # print("message content" , self.send_message(content))
         self.send_message(content)
 
     def new_message(self , data):
@@ -32,14 +33,14 @@ class ChatConsumer(WebsocketConsumer):
             'command' : 'new_message',
             'message' : self.message_to_json(message)
         }
-        return self.send_chat_message(content)
+        self.send_chat_message(content)
 
 
     def messages_to_json(self , messages):
         result = []
         for message in messages:
             result.append(self.message_to_json(message))
-        print("Result in json list" , result[0])
+        # print("Result in json list" , result[0])
         return result
     
     def message_to_json(self , message):
@@ -89,9 +90,13 @@ class ChatConsumer(WebsocketConsumer):
         )
 
     def send_message(self , message):
+        # print('message in in send_message', message)
+        # print('ccc', self.send(text_data=json.dumps(message)))
         self.send(text_data=json.dumps(message))
+
 # Receive message from room group
     def chat_message(self, event):
+        print('event' , event)
         message = event['message']
         # Send message to WebSocket
         return self.send(text_data=json.dumps(message))
